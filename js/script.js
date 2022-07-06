9b7ff44fba0ce3c522ad2b07b71e7948ffce1f92
Gleb Glushchenko <darkvee1n@ya.ru>
2022-07-06 10:12:47 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const checkbox = document.querySelector(".theme-switch__checkbox");
+
+checkbox.addEventListener("change", function () {
+  transition();
+  if (this.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+});
+
+const transition = () => {
+  document.documentElement.classList.add("transition");
+  setTimeout(() => {
+    document.documentElement.classList.remove("transition");
+  }, 250);
+};
